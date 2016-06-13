73045b600af1aea6cf06a2bab3363b38a7d12fd5
Ben Kimock <kimockb@gmail.com>
2016-06-12 20:25:05 -0400

    adding numexpr example

--- examples/speed_examples.py
+++ examples/speed_examples.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import numba
+import numexpr as ne
 
 
 def loop_sum(arr):
@@ -29,6 +30,20 @@ def numba_sum(arr):
     return sum
 
 
+def numpy_expression(arr):
+    return 2*arr + 3*arr + 4*arr + 5*arr
+
+
+def numexpr_expression(arr):
+    return ne.evaluate('2*arr + 3*arr + 4*arr + 5*arr')
+
+
+@numba.jit(nopython=True)
+def numba_expression(arr):
+    return 2*arr + 3*arr + 4*arr + 5*arr
+
+
+
 def speed_test(function, input, repeats=20):
     start = time.time()
     for repeat in range(repeats):
@@ -40,14 +55,19 @@ if __name__ == '__main__':
     test = np.empty((4096, 4096))
 
     print('Simple loop time:', speed_test(loop_sum, test, repeats=1))
-
     print('Numpy time:', speed_test(numpy_sum, test))
+    print()
 
     print('Initial crude numba time:', speed_test(crude_numba_sum, test, repeats=1))
-
     print('Later crude numba time:', speed_test(crude_numba_sum, test))
+    print()
 
     print('Initial numba time:', speed_test(numba_sum, test, repeats=1))
-
     print('Later numba time:', speed_test(numba_sum, test))
+    print()
 
+    print('Long expression examples examples:')
+    print('Normal Python/numpy:', speed_test(numpy_expression, test))
+    print('Initial numba expression:', speed_test(numba_expression, test, repeats=1))
+    print('Later numba expression:', speed_test(numba_expression, test))
+    print('Numexpr:', speed_test(numexpr_expression, test))
